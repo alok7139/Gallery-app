bfe766092612336cd51ea86d555b288b34d364c0
alok7139 <alokgarg7139@gmail.com>
2024-12-16 21:30:44 +0530

    commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,10 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div className='p-3 bg-blue-500 text-red-400'> 
+      <div className='p-3 m-16 bg-blue-500 rounded-md text-red-400'> 
          alok garg
       </div>
       
