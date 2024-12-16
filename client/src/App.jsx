44a9d6e857f1d03b3a905e3aa6c531e7fc966581
alok7139 <alokgarg7139@gmail.com>
2024-12-16 21:27:32 +0530

    commit

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='p-3 bg-blue-500 text-red-400'> 
+         alok garg
+      </div>
+      
+    </>
+  )
+}
+
+export default App
